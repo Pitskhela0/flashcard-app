91f2f31a8ab619b1d0cc071eea86b3827455e5fb
oto812 <abashidzeoto@gmail.com>
2025-04-19 22:55:04 +0400

    defined interfaces for for API data structures

---
+++ backend/src/types/index.ts
@@ -0,0 +1,39 @@
+import { Flashcard, AnswerDifficulty, BucketMap } from '@logic/flashcards';
+
+export interface PracticeSession {
+    cards: Flashcard[], 
+    day: number
+}
+
+export interface UpdateRequest {
+    cardFront: string; 
+    cardBack: string; 
+    difficulty: AnswerDifficulty; 
+  }
+
+
+  export interface HintRequest {
+    cardFront: string; 
+    cardBack: string;  
+  }
+
+
+
+
+export interface ProgressStats {
+  totalCards: number;
+  masteredCards: number;
+  progress: number; 
+  totalCorrect: number; 
+  totalAnswered: number; 
+  correctnessRate: number; 
+}
+
+export interface PracticeRecord {
+    cardFront: string;
+    cardBack: string;
+    timestamp: number; 
+    difficulty: AnswerDifficulty;
+    previousBucket: number;
+    newBucket: number;
+  }
